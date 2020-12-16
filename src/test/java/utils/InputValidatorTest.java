93e18222e5009e6599ac049496854ac6c5df501a
sori <bosl95@naver.com>
2020-12-16 16:28:13 +0900

    style : 단위테스트 기능 분리

--- src/test/java/utils/InputValidatorTest.java
+++ src/test/java/utils/InputValidatorTest.java
@@ -8,23 +8,31 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputValidatorTest {
     @Test
-    void validNameInput() {
-        String input = ",이름1";
+    void 맨_앞에_구분자_입력() {
         assertThrows(InvalidCarNameException.class ,
-                () -> InputValidator.validNameInput(input));
+                () -> InputValidator.validNameInput(",이름1"));
+    }
 
+    @Test
+    void 맨_뒤에_구분자_입력() {
         assertThrows(InvalidCarNameException.class ,
                 () -> InputValidator.validNameInput("이름1,이름2,"));
+    }
 
+    @Test
+    void 공백_입력() {
         assertThrows(InvalidCarNameException.class ,
                 () -> InputValidator.validNameInput(" "));
+    }
 
+    @Test
+    void 이름_사이에_중복_입력() {
         assertThrows(InvalidCarNameException.class ,
                 () -> InputValidator.validNameInput("이름1, 이름2,이름3"));
     }
 
     @Test
-    void isDistinctName() {
+    void 이름이_중복되는_경우() {
         assertThrows(DuplicateCarNameException.class,
                 () -> InputValidator.isDistinctName("이름1,이름1,이름2"));
     }
